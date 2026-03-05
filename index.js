f146a940fdf9effeb0fae39b80e56be185ecf94f
kajal gohil <kajalgohil2112@gmail.com>
2026-03-05 18:48:44 +0530

    for access image form uploads folder change in index.js file

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ import quoteRoutes from "./routes/quoteRoutes.js";
 import customerRoutes from "./routes/customerRoutes.js";
 import productsRoutes from "./routes/productsRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -16,6 +21,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // routes
 app.use("/auth", authRoutes);
